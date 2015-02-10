8b497309978326d20880b6b25c5f2087c85fa731
dineshygv <dinesh.ygv@gmail.com>
2015-02-10 21:47:59 +0530

    Project init

---
+++ AzureBlobStorageProto/AzureStorageCRUD.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Configuration;
+
+namespace AzureBlobStorageProto
+{
+    public class AzureStorageCRUD
+    {
+        private CloudBlobClient blobClient;
+
+        public AzureStorageCRUD()
+        {
+            // Retrieve storage account from connection string.
+            var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+
+            // Create the blob client.
+            blobClient = storageAccount.CreateCloudBlobClient();
+        }
+
+        public CloudBlobContainer GetContainer(string containerName)
+        {            
+            // Retrieve a reference to a container. 
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+            // Create the container if it doesn't already exist.
+            container.CreateIfNotExists();
+
+            return container;
+        }
+
+        public CloudBlockBlob UploadFile(CloudBlobContainer container, string blobName, string localFilePath)
+        {
+            // Retrieve reference to a blob with given blob name
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(blobName);
+
+            // Create or overwrite the blob with contents from a local file.
+            using (var fileStream = System.IO.File.OpenRead(localFilePath))
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+
+            return blockBlob;
+        }
+        
+    }
+}
---
+++ AzureBlobStorageProto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AzureBlobStorageProto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var crud = new AzureStorageCRUD();
+            var container = crud.GetContainer("testcontainer");
+            var file = crud.UploadFile(container, "testfile", @"D:\testfile.txt");
+            Console.ReadKey();
+        }
+    }
+}
